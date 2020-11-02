ef4131e27d94295f07dd7acd2f9e10883f28dd6b
GOJAL9\09080381 <Weigo@etainfo.com.tw>
2020-11-02 13:32:50 +0800

    update article

--- src/components/Content/ArticleList/PostPreview.vue
+++ src/components/Content/ArticleList/PostPreview.vue
@@ -36,7 +36,7 @@ export default {
         {
           thumbnail:
             "https://cdn.substack.com/image/fetch/w_336,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F53fbe6f1-2ffc-4b71-9e77-be638c838233_1180x984.jpeg",
-          title: "W-Reading - 為什麼感覺時間會過的快?",
+          title: "W-Reading - 關於時間管理，我的五大訣竅?",
           description: "#2 以及一些“技術哲學”問題",
           author: "W-Reading",
           time: "2020-10-24",
